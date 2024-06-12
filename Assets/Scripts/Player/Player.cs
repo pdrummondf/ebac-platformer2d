848aa1dc1fb13250c0e1d8c7cac24eafc6816a66
pdrummondf <pdrummondf@gmail.com>
2024-06-11 22:15:19 -0300

    Adicionando pulo e removendo atrito das paredes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rbJogador;
-    public float velocidade = 15f;
+    public float velocidade = 5;
+    public float forcaPulo = 2;
+    public Vector2 atrito = new Vector2(.1f, 0);
 
 
     public void Update()
+    {
+        HandlePulo();
+        HandleMovimentos();
+    }
+
+    private void HandleMovimentos()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -16,7 +24,24 @@ public class Player : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            rbJogador.velocity = new Vector2(velocidade, rbJogador.velocity.y);            
+            rbJogador.velocity = new Vector2(velocidade, rbJogador.velocity.y);
+        }
+
+        if (rbJogador.velocity.x > 0)
+        {
+            rbJogador.velocity -= atrito;
+        }
+        else if (rbJogador.velocity.x < 0)
+        {
+            rbJogador.velocity += atrito;
+        }
+    }
+
+    private void HandlePulo()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbJogador.velocity = Vector2.up * forcaPulo;
         }
     }
 }
